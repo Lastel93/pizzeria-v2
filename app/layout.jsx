0fd4ed0088ab34b5a0c4ad6dc3ee614777661dd4
Lastel93 <ste.pifferi@gmail.com>
2026-06-17 18:44:04 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,9 @@
 import './globals.css'
 
-export const metadata = {
-  title: 'Pizzeria dai Burini - Menu Digitale',
-  description: 'Le nostre pizze artigianali direttamente a casa tua',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
-      <body>{children}</body>
+      <body className="bg-[#fcfbf7]">{children}</body>
     </html>
   )
 }
