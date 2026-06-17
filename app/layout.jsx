3f1acbfe5ab394b2eff9350c9e854278a72b0281
Lastel93 <ste.pifferi@gmail.com>
2026-06-17 19:08:37 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
-      <body className="bg-[#fcfbf7]">{children}</body>
+      <body className="bg-[#fdfcf7] antialiased m-0 p-0">{children}</body>
     </html>
   )
 }
